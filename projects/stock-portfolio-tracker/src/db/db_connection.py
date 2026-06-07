0975057343f26453d8c171e6c24864a5fea434b3
Brandon Ma <brandon2017ma@gmail.com>
2026-06-07 13:22:23 +0800

    use fallback approach in db connection

--- projects/stock-portfolio-tracker/src/db/db_connection.py
+++ projects/stock-portfolio-tracker/src/db/db_connection.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 import streamlit as st
+import os
 
-DB_USER = st.secrets["DB_USER"]
-DB_PASSWORD = st.secrets["DB_PASSWORD"]
-DB_HOST = st.secrets["DB_HOST"]
-DB_PORT = st.secrets["DB_PORT"]
-DB_NAME = st.secrets["DB_NAME"]
+load_dotenv()
+
+DB_USER = st.secrets.get("DB_USER", os.getenv("DB_USER"))
+DB_PASSWORD = st.secrets.get("DB_PASSWORD", os.getenv("DB_PASSWORD"))
+DB_HOST = st.secrets.get("DB_HOST", os.getenv("DB_HOST"))
+DB_PORT = st.secrets.get("DB_PORT", os.getenv("DB_PORT"))
+DB_NAME = st.secrets.get("DB_NAME", os.getenv("DB_NAME"))
 
 DATABASE_URL = (
     f"postgresql://{DB_USER}:{DB_PASSWORD}"
