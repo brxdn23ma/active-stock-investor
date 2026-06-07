2b582de3123e491fcaa9a30f10a038e2b8d17738
Brandon Ma <brandon2017ma@gmail.com>
2026-06-07 22:49:06 +0800

    temporary db connection file change for debugging

--- projects/stock-portfolio-tracker/src/db/db_connection.py
+++ projects/stock-portfolio-tracker/src/db/db_connection.py
@@ -2,4 +2,10 @@ from sqlalchemy import create_engine
 import streamlit as st
 
 DATABASE_URL = st.secrets["DATABASE_URL"]
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+print("USING DATABASE:", DATABASE_URL[:60])
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
