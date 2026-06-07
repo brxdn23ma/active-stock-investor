71234ed7e87515583147b61e7bb41a4d9bd0bbcc
Brandon Ma <brandon2017ma@gmail.com>
2026-06-07 13:09:16 +0800

    new db connection file content

--- projects/stock-portfolio-tracker/src/db/db_connection.py
+++ projects/stock-portfolio-tracker/src/db/db_connection.py
@@ -1,20 +1,15 @@
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
-import os
+import streamlit as st
 
-load_dotenv()
-
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = st.secrets["DB_USER"]
+DB_PASSWORD = st.secrets["DB_PASSWORD"]
+DB_HOST = st.secrets["DB_HOST"]
+DB_PORT = st.secrets["DB_PORT"]
+DB_NAME = st.secrets["DB_NAME"]
 
 DATABASE_URL = (
     f"postgresql://{DB_USER}:{DB_PASSWORD}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
-
-print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
